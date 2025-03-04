d9bff221c7191ea2209ba333317642c7f1918738
TheCrowStudios <madgamersmemes@gmail.com>
2025-03-04 10:33:21 +0000

    redirect user to game menu instead of game after logging in

--- scripts/login.js
+++ scripts/login.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (inpUsername.value === 'user' && inpPassword.value === 'password') {
             document.cookie = `username=${inpUsername.value}; Secure;`;
             console.log(`cookie: ${document.cookie}`);
-            document.location = '/game';
+            document.location = '/game-menu';
         }
         else {
             error.textContent = 'Username or password incorrect';
